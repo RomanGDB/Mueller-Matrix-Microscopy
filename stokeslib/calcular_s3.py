411983bbb03e571058fce7e6eeb0e5fa76a01d21
RomanGDB <140636406+RomanGDB@users.noreply.github.com>
2024-10-30 20:36:58 -0300

    calcular s3

---
+++ stokeslib/calcular_s3.py
@@ -0,0 +1,10 @@
+def calcular_S3(S_in, DoP, signos):
+
+  # S3 Modulo
+  S_in[:,:,:,3,:] = np.sqrt(DoP**2 * S_in[:,:,:,0,:].astype(float)**2 - S_in[:,:,:,1,:].astype(float)**2 - S_in[:,:,:,2,:].astype(float)**2)
+
+  # S3 Signo
+  for i, signo in enumerate(signos):
+    S_in[:,:,:,3,i] = signo * S_in[:,:,:,3,i]
+
+  return S_in
